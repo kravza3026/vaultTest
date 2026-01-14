c586e37adc9add4227e253d65b605d3e0222175c
kravza3026 <kravza3026@gmail.com>
2026-01-14 16:39:57 +0200

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Header } from './components/Header'
-import { Footer } from './components/Footer'
 import graph from '@/assets/images/bg_graph.svg'
 import './App.css'
 import {WelcomeSection} from "@/components/WelcomeSection";
